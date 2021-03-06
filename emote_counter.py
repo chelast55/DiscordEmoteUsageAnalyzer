bb453cc2d4271e3f68df851f83d7ed906bb8b758
chelast55 <tim@amoto.de>
2021-03-06 13:13:40 +0100

    Add support for optional inclusion of vanilla/unicode emoticons

--- emote_counter.py
+++ emote_counter.py
@@ -2,35 +2,54 @@ import csv
 import os
 import re
 
+vanilla_emote_filepath = 'res/vanilla.csv'
+custom_emote_filepath = 'res/custom.csv'
 
-def run(input, output, filename):
 
+def run(input_directory, output_directory, filename):
+    run_vanilla_check(input_directory, output_directory, filename, False)
+
+
+def run(input_directory, output_directory, filename, include_vanilla_emotes):
+    run_vanilla_check(input_directory, output_directory, filename, include_vanilla_emotes)
+
+
+def run_vanilla_check(input_directory, output_directory, filename, include_vanilla_emotes):
     # collect desired emotes
     emotes = []
-    with open('res/emotes.csv') as emote_file:
+    if include_vanilla_emotes:
+        with open(vanilla_emote_filepath, encoding="utf8") as emote_file:
+            emote_reader = csv.reader(emote_file)
+            for row in emote_reader:
+                emotes += row
+    with open(custom_emote_filepath, encoding="utf8") as emote_file:
         emote_reader = csv.reader(emote_file)
         for row in emote_reader:
             emotes += row
 
     # import discord channel text files
     text_channels = []
-    for file in os.listdir(input):
+    for file in os.listdir(input_directory):
         if file.endswith(".txt"):
-            with open(input + file, "r", encoding="utf8") as text_file:
+            with open(input_directory + file, "r", encoding="utf8") as text_file:
                 text_channels += [text_file.read()]
 
     # count emote uses
     text_uses = [0] * len(emotes)
     react_uses = [0] * len(emotes)
     for e in range(0, len(emotes)):
-        in_text = re.compile(rf":{emotes[e]}:")
+        # unicode emotes are never surrounded by colons in discord-chat-exporter's .txt format
+        if include_vanilla_emotes and len(emotes[e]) == 1:
+            in_text = re.compile(rf"{emotes[e]}")
+        else:
+            in_text = re.compile(rf":{emotes[e]}:")
         in_react = re.compile(rf"{{Reactions}}\n.*{emotes[e]} ")
         for t in range(0, len(text_channels)):
             text_uses[e] += len(re.findall(in_text, text_channels[t]))
             react_uses[e] += len(re.findall(in_react, text_channels[t]))
 
     # print csv (usage count)
-    with open(output + filename, 'w+', newline='') as emote_usage_file:
+    with open(output_directory + filename, 'w+', newline='', encoding="utf8") as emote_usage_file:
         emote_usage_writer = csv.writer(emote_usage_file, delimiter=';')
         emote_usage_writer.writerow(['emote'] + ['text_uses'] + ['react_uses'] + ['total_uses'])
         for e in range(0, len(emotes)):
--- main.py
+++ main.py
@@ -6,5 +6,11 @@ output_directory = 'out/'
 
 if __name__ == '__main__':
 
-    emote_counter.run(input_directory, output_directory, 'emote_usage.csv')
-    date_splitter.run(input_directory)
+    # generate .csv file logging custom emote use of all discord channel text documents in input_directory combined
+    #emote_counter.run(input_directory, output_directory, 'emote_usage.csv')
+
+    # generate .csv file logging emote use of all discord channel text documents in input_directory combined
+    emote_counter.run(input_directory, output_directory, 'emote_usage.csv', True)
+
+    # split all discord channel text documents in input_directory by month in chronological order
+    #date_splitter.run(input_directory)
